18569a47002db8a8d12c542f025a381dc419da2b
Michael Skergeth <skemib14@htlkaindorf.at>
2020-07-05 16:54:01 +0200

    User Eingabe

--- src/at/campus02/nowa/uno/App.java
+++ src/at/campus02/nowa/uno/App.java
@@ -13,7 +13,7 @@ public class App {
     private UnoKarte currentCard;
 
 
-    public App(Scanner input, PrintStream output){
+    public App(Scanner input, PrintStream output) {
         this.input = input;
         this.output = output;
     }
@@ -35,20 +35,20 @@ public class App {
             } while (!gameEnded());
 
             printFinalScore();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             output.println(ex);
         }
     }
 
     private void initializeGame() { // todo: Spielernamen eingeben 4x, Karten mischen,
-                                    // 1 UnoKarte aufdecken, Aktionskarte prüfen, Anzeige Spieler
+        // 1 UnoKarte aufdecken, Aktionskarte prüfen, Anzeige Spieler
 
         kartenDeck.makeDeck();
         kartenDeck.shuffleDeck();
 
         // Spieler eingeben
-        for(int i = 1; i <= 4;i++){
+        for (int i = 1; i <= 4; i++) {
             System.out.println("Name Spieler " + i + ": ");
             //Scanner scan = new Scanner(System.in);
             String name = input.next();
@@ -69,108 +69,127 @@ public class App {
 
     public void chooseRandomPlayer() {  // zufälligen Spieler bestimmen
         Random random = new Random();
-        int max = 4;
-        int min = 1;
-        indexCurrentSpieler = random.nextInt((max-min) + 1) + min;
+        int max = 3;
+        int min = 0;
+        indexCurrentSpieler = random.nextInt((max - min) + 1) + min;
         System.out.println(spielerListe.toString());
         System.out.println("Startspieler: " + indexCurrentSpieler);
     }
 
-    public void addSpieler(Spieler s){
+    public void addSpieler(Spieler s) {
         spielerListe.add(s);
     }
 
-    public void verteileHandkarten(){
-        for(Spieler s : spielerListe){
+    public void verteileHandkarten() {
+        for (Spieler s : spielerListe) {
             s.setHandKarten(kartenDeck.makePlayerDeck());
         }
     }
 
-    public void startKarteFestlegen(){
+    public void startKarteFestlegen() {
 
         currentCard = kartenDeck.spielKartenDeck.remove();
     }
 
     private void readUserInput() {
 
-        System.out.println("bitte gib deine Karte bekannt.");
 
-        String s = input.next();
-        String values[] = s.split("-");
         Farbe f = null;
         Kartenwert kW = null;
         boolean korrekteEingabe = false;
-        do{
-        switch (values[0]){
-            case "R": f= Farbe.ROT;
-            break;
-            case "B": f = Farbe.BLAU;
-            break;
-            case "G": f = Farbe.GRÜN;
-            break;
-            case "Y": f = Farbe.YELLOW;
-            break;
-            case "S": f = Farbe.SCHWARZ;
-            break;
-        }
-        switch (values[1]) {
-            case "0":
-                kW = Kartenwert.ZERO;
-                break;
-            case "1":
-                kW = Kartenwert.EINS;
-                break;
-            case "2":
-                kW = Kartenwert.ZWEI;
-                break;
-            case "3":
-                kW = Kartenwert.DREI;
-                break;
-            case "4":
-                kW = Kartenwert.VIER;
-                break;
-            case "5":
-                kW = Kartenwert.FÜNF;
-                break;
-            case "6":
-                kW = Kartenwert.SECHS;
-                break;
-            case "7":
-                kW = Kartenwert.SIEBEN;
-                break;
-            case "8":
-                kW = Kartenwert.ACHT;
-                break;
-            case "9":
-                kW = Kartenwert.NEUN;
-                break;
-            case "RW":
-                kW = Kartenwert.RW;
-                break;
-            case "OUT":
-                kW = Kartenwert.OUT;
-                break;
-            case "+2":
-                kW = Kartenwert.PLUS2;
-                break;
-            case "+4":
-                kW = Kartenwert.PLUS4;
-                break;
-            case "WILD":
-                kW = Kartenwert.WILD;
-                break;
-        }
-        if (f == null || kW == null) {
-            UnoKarte u = new UnoKarte(f, kW);
+        do {
+            System.out.println("bitte gib deine Karte bekannt.");
 
-            if (spielerListe.get(indexCurrentSpieler).getHandKarten().contains(u)) {
-                ablagestapel.add(u);
-                spielerListe.get(indexCurrentSpieler).getHandKarten().remove(u);
-                korrekteEingabe = true;
+            String s = input.next();
+            String values[] = s.split("-");
 
+            switch (values[0]) {
+                case "R":
+                    f = Farbe.ROT;
+                    break;
+                case "B":
+                    f = Farbe.BLAU;
+                    break;
+                case "G":
+                    f = Farbe.GRÜN;
+                    break;
+                case "Y":
+                    f = Farbe.YELLOW;
+                    break;
+                case "S":
+                    f = Farbe.SCHWARZ;
+                    break;
             }
-        }
-        } while(!korrekteEingabe);
+            switch (values[1]) {
+                case "0":
+                    kW = Kartenwert.ZERO;
+                    break;
+                case "1":
+                    kW = Kartenwert.EINS;
+                    break;
+                case "2":
+                    kW = Kartenwert.ZWEI;
+                    break;
+                case "3":
+                    kW = Kartenwert.DREI;
+                    break;
+                case "4":
+                    kW = Kartenwert.VIER;
+                    break;
+                case "5":
+                    kW = Kartenwert.FÜNF;
+                    break;
+                case "6":
+                    kW = Kartenwert.SECHS;
+                    break;
+                case "7":
+                    kW = Kartenwert.SIEBEN;
+                    break;
+                case "8":
+                    kW = Kartenwert.ACHT;
+                    break;
+                case "9":
+                    kW = Kartenwert.NEUN;
+                    break;
+                case "RW":
+                    kW = Kartenwert.RW;
+                    break;
+                case "OUT":
+                    kW = Kartenwert.OUT;
+                    break;
+                case "+2":
+                    kW = Kartenwert.PLUS2;
+                    break;
+                case "+4":
+                    kW = Kartenwert.PLUS4;
+                    break;
+                case "WILD":
+                    kW = Kartenwert.WILD;
+                    break;
+            }
+            if (f != null && kW != null) {
+                //UnoKarte u =  new UnoKarte(f, kW);
+                UnoKarte u = spielerListe.get(indexCurrentSpieler).getKarte(kW, f);
+
+                if (u != null) {
+
+                    if (spielerListe.get(indexCurrentSpieler).getHandKarten().contains(u)) {
+                        ablagestapel.add(u);
+                        spielerListe.get(indexCurrentSpieler).getHandKarten().remove(u);
+                        System.out.println("Anzahl Handkarten: " + spielerListe.get(indexCurrentSpieler).getHandKarten().size());
+                        korrekteEingabe = true;
+
+                    }
+                }
+//            if (spielerListe.get(indexCurrentSpieler).hasCard(u)){
+//                ablagestapel.add(u);
+//                spielerListe.get(indexCurrentSpieler).getHandKarten().remove(u);
+//                System.out.println("Anzahl Handkarten: "+spielerListe.get(indexCurrentSpieler).getHandKarten().size());
+//                korrekteEingabe = true;
+//
+//            }
+            }
+        } while (!korrekteEingabe);
 
 
     }
@@ -187,17 +206,17 @@ public class App {
         System.out.println("Deine aktuellen Karten:" + s.getHandKarten());
     }
 
-    private boolean roundEnded(){
+    private boolean roundEnded() {
 
         return false;
     }
 
-    private boolean gameEnded(){
+    private boolean gameEnded() {
 
         return false;
     }
 
-    private void printFinalScore(){
+    private void printFinalScore() {
 
     }
 }
