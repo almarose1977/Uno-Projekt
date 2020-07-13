7f87b4a65ae2e947ef7737f26d6e31148c5566a0
almarose1977 <almarose1977@gmail.com>
2020-07-13 11:35:21 +0200

    test neuer laptop

--- src/at/campus02/nowa/uno/App.java
+++ src/at/campus02/nowa/uno/App.java
@@ -13,12 +13,18 @@ public class App {
     private LinkedList<UnoKarte> stack = new LinkedList<>();        // Ablagestapel
     private ArrayList<Spieler> playersList = new ArrayList<>();
     private int indexCurrentSpieler;
+    private int test1;
+
+    public void test() {
+        test1 = 15;
+    }
 
 
     public App(Scanner input, PrintStream output) {
         this.input = input;
         this.output = output;
     }
+
 
     public void Run() {
         initializeGame();
@@ -351,6 +357,7 @@ public class App {
                     break;*/
             }
             stack.getLast().setFARBE(Farbe.valueOf(farbwunsch));
+
         } catch (IllegalArgumentException e) {
             System.out.println("Falsche Eingabe");
         }
