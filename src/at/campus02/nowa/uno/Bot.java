1dda88bec749cda172cf521dad3422f6f80c5bc7
almarose1977 <almarose1977@gmail.com>
2020-07-17 13:26:04 +0200

    Bot + Mensch integriert, makeTurn statt readUserInput

---
+++ src/at/campus02/nowa/uno/Bot.java
@@ -0,0 +1,23 @@
+package at.campus02.nowa.uno;
+
+public class Bot extends Spieler {
+
+    public Bot(String name) {
+        super(name);
+    }
+
+    public UnoKarte getFirstValidCard(UnoKarte previousCard) {
+        for (UnoKarte u : getHandCardDeck()) {
+            if (App.validCard(u, previousCard))
+                return u;
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Bot Name: " + getName() + ", Punkte = " + getPoints() +
+                ", Handkarten: " + getHandCardDeck() + '\n';
+    }
+}
